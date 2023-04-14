25330d4c452a4e3c3cd13406d2547cd4f696cbae
sumitshinde-84 <shindesumit217@gmail.com>
2023-04-14 14:16:45 +0530

    add experience js logic

--- src/components/Form_Component/experience.js
+++ src/components/Form_Component/experience.js
@@ -10,16 +10,16 @@ class Experience extends React.Component{
 
 
 render(){
-
+    const {experience,onChangeInputExperience,experienceId} = this.props
     return(
+       
         <div className="experience">
             <h2>Experience</h2>
-            <input className="input" type="text" id="position" placeholder="Postition"/>
-            <input className="input" type="text" id="Company" placeholder="Company"/>
-            <input className="input" type="text" id="From" placeholder="From"/>
-            <input className="input" type="text" id="To" placeholder="To"/>
-            <button className="add">Add</button>
-            <button className="delete">Delete</button>
+            <input className="input" type="text" id={experienceId} name="position" placeholder="Postition" value={experience.position} onChange={(event)=>{onChangeInputExperience(event)}}/>
+            <input className="input" type="text"  id={experienceId} name="company" placeholder="Company" value={experience.company} onChange={(event)=>{onChangeInputExperience(event)}}/>
+            <input className="input" type="text"  id={experienceId} name="from" placeholder="From" value={experience.from} onChange={(event)=>{onChangeInputExperience(event)}}/>
+            <input className="input" type="text"  id={experienceId} name="to" placeholder="To" value={experience.to} onChange={(event)=>{onChangeInputExperience(event)}}/>
+          
            
 
         </div>
