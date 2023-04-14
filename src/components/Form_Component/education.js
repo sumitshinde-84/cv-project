a1a3c967041d0cd80e94f637041a5fbf7af6b923
sumitshinde-84 <shindesumit217@gmail.com>
2023-04-14 14:17:15 +0530

    add education js logic

--- src/components/Form_Component/education.js
+++ src/components/Form_Component/education.js
@@ -6,18 +6,17 @@ class Education extends React.Component{
 
 render(){
 
-
+    const {education,educationId,onChangeInputEducation} = this.props
     return(
         <div className="education">
             <h2>Education</h2>
-          <input className="input"  id="Univercity_name" type="text" placeholder="University Name"/>
-          <input className="input"  id="City" type="text" placeholder="City"/>
-          <input className="input"  id="Degree" type="text" placeholder="Degree"/>
-          <input className="input"  id="Subject" type="text" placeholder="Subject"/>
-          <input className="input"  id="From" type="text" placeholder="From"/>
-          <input className="input"  id="To" type="text" placeholder="To"/>
-          <button className="add">Add</button>
-          <button className="delete">Delete</button>
+          <input className="input"  id={educationId} name="university" type="text"  placeholder="University Name" value={education.university} onChange={(event)=>{onChangeInputEducation(event)}}/>
+          <input className="input"  id={educationId} type="text" name="city" placeholder="City"  value={education.city} onChange={(event)=>{onChangeInputEducation(event)}}/>
+          <input className="input" id={educationId} type="text" name="degree" placeholder="Degree"  value={education.degree} onChange={(event)=>{onChangeInputEducation(event)}}/>
+          <input className="input" id={educationId} type="text" name="subject" placeholder="Subject" value={education.subject} onChange={(event)=>{onChangeInputEducation(event)}}/>
+          <input className="input"  id={educationId} type="text" name="from" placeholder="From" value={education.from} onChange={(event)=>{onChangeInputEducation(event)}}/>
+          <input className="input"  id={educationId} type="text" name="to" placeholder="To" value={education.to} onChange={(event)=>{onChangeInputEducation(event)}}/>
+          
         </div>
     )
 }
--- src/components/Form_Component/personalInfo.js
+++ src/components/Form_Component/personalInfo.js
@@ -1,26 +1,32 @@
+
 import React from "react";
+import Experience from "./experience";
 import "./style/personal_info.css"
 
 class PersonlInfo extends React.Component{
+
+constructor(props){
+    super()
+}
 
 
 
 
 
 render(){
-
+    const {personalInfo,onChangeInputPersonalInfo} = this.props
 
     return(
         <div className="personal_info">
             <h2>Personl Information</h2>
-            <input type="text" className="input" id="FirstName" placeholder="First Name"/>
-            <input type="text" className="input" id="LastName" placeholder="Last Name"/>
-            <input type="text" className="input" id="Title" placeholder="Title"/>
-            <input class="custom-file-input" type="file"/>
-            <input type="text" className="input" id="Address" placeholder="Address"/>
-            <input type="tel" className="input" id="Phone" placeholder="Phone number"/>
-            <input type="email" className="input" id="Email" placeholder="Email"/>
-            <input type="text" className="input" id="Description" placeholder="Description"/>
+            <input type="text" className="input" id="FirstName"  name="firstName" placeholder="First Name" value={personalInfo.firstName} onChange={(event) => onChangeInputPersonalInfo(event)} />           
+            <input type="text" className="input" id="LastName"  name="lastName"placeholder="Last Name" value={personalInfo.lastName} onChange={(event) => onChangeInputPersonalInfo(event)}/>
+            <input type="text" className="input" id="Title" placeholder="Title" name="title" value={personalInfo.title} onChange={(event)=>{onChangeInputPersonalInfo(event)}}/>
+            <input  id="File" class="custom-file-input" type="file"    onChange={(event)=>{onChangeInputPersonalInfo(event)}} accept="image/jpeg image/png"/>
+            <input type="text" className="input" id="Address"  name="address" placeholder="Address"  value={personalInfo.address} onChange={(event)=>{onChangeInputPersonalInfo(event)}}/>
+            <input type="tel" className="input" id="Phone"  name="phone" placeholder="Phone number" value={personalInfo.phone} onChange={(event)=>{onChangeInputPersonalInfo(event)}}/>
+            <input type="email" className="input" id="Email" name="email" placeholder="Email" value={personalInfo.email} onChange={(event)=>{onChangeInputPersonalInfo(event)}}/>
+            <input type="text" className="input" id="Description" name="description" placeholder="Description" value={PersonlInfo.description} onChange={(event)=>{onChangeInputPersonalInfo(event)}}/>
             
         </div>
     )
